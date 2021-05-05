3022788ce7af09b1e38af40d6fb46fccf5498443
bidurkandel <kandelbidur11@gmail.com>
2021-05-05 16:22:14 -0500

    Done with my project

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const path = require('path')
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+server.use(express.static(path.join(__dirname, 'client/build')))
+
+console.log(process.env.NODE_ENV)
+
+
+if (process.env.NODE_ENV === 'development') { 
+    const cors = require('cors')
+    server.use(cors())
+}
+
+
+server.get('/api/hello', (req, res) => {
+    res.json({ message: 'hello' })
+})
+
+
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
+
+const PORT = process.env.PORT || 4000
+
+server.listen(PORT, () => {
+    console.log (`Listening on ${PORT}`)
+})
